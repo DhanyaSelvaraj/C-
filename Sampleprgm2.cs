a1eb2a96325940f560dbd49593b2c50953634e38
Dhanya Rajakumari <34230161+DhanyaSelvaraj@users.noreply.github.com>
2018-05-02 15:34:36 +0530

    Add files via upload

---
+++ Sampleprgm2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        Console.WriteLine("Enter your name: ");
+        string UserName = Console.ReadLine();
+        Console.WriteLine("hello {0}", username);
+	}
+}
+ 
